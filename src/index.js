c5b015eac6cf263d244b4c33d1e27f0b0dba98ac
SvetlanaForte-2000 <105925303+SvetlanaForte-2000@users.noreply.github.com>
2025-08-19 21:53:35 +0300

    fix: correct formatting issues in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function towelSort(/* matrix */) {
-  throw new Error('Not implemented');
+module.exports = function towelSort(matrix) {
+  if (!matrix || matrix.length === 0) return [];
+  return matrix.reduce((acc, row, index) => {
+    return index % 2 === 0 ? acc.concat(row) : acc.concat(row.reverse());
+  }, []);
 };
